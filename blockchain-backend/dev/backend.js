1eb71b47ff7c699989f0f2ea1dfd0545c9bcfe21
anil0403 <anilshrestha0403.as@gmail.com>
2023-01-08 17:53:04 +0545

    database connect

--- blockchain-backend/dev/backend.js
+++ blockchain-backend/dev/backend.js
@@ -1,10 +1,12 @@
 var mysql = require('mysql');
 
 var con = mysql.createConnection({
+  port :3306,
   host: "localhost",
   user: "root",
   password: "password",
   database: "testdb",
+  connectionLimit : 10,
   insecureAuth : true
 });
 
